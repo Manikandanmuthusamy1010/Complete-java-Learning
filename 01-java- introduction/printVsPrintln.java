5d012f7f7c920f92584ee52471b426a7235cd686
Manikandanmuthusamy1010 <manikandanmuthu1010@gmail.com>
2026-03-10 08:05:39 +0530

    Create printVsPrintln.java

---
+++ 01-java- introduction/printVsPrintln.java
@@ -0,0 +1,15 @@
+public class PrintVsPrintln {
+    public static void main(String[] args) {
+
+        System.out.print("Hello ");
+        System.out.print("World ");
+
+        System.out.println("This is println");
+
+        System.out.print("Java ");
+        System.out.println("Programming");
+
+        System.out.print("Line 1 ");
+        System.out.println("Line 2");
+    }
+}
