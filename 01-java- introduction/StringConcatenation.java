dccde674ff89d949bd172d502139faf7e99c9fb6
Manikandanmuthusamy1010 <manikandanmuthu1010@gmail.com>
2026-03-11 15:22:32 +0530

    Create StringConcatenation.java

---
+++ 01-java- introduction/StringConcatenation.java
@@ -0,0 +1,8 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String name = "Java";
+        int version = 17;
+
+        System.out.println("Learning " + name + " version " + version);
+    }
+}
