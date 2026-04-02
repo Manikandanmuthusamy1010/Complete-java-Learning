9e50fce51a52a80ceb3dc8f7f2aae5cc0b4f3d06
Manikandanmuthusamy1010 <manikandanmuthu1010@gmail.com>
2026-04-02 14:45:03 +0530

    Create 1-Primitive.java

---
+++ 02-Operators/03-DataTypes/1-Primitive.java
@@ -0,0 +1,29 @@
+public class Primitive {
+    public static void main(String[] args) {
+
+        
+        byte b = 100;          // 1 byte
+        short s = 10000;       // 2 bytes
+        int i = 100000;        // 4 bytes
+        long l = 10000000000L; // 8 bytes
+
+        
+        float f = 10.5f;       // 4 bytes
+        double d = 20.99;      // 8 bytes
+
+      
+        char c = 'A';          // 2 bytes (single character)
+
+        
+        boolean bool = true;   // true or false
+
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + bool);
+    }
+}
